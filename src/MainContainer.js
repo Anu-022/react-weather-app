068772241d1d49cb54b77401ae100c30e3965d8d
Anuoluwapo <ilorianuoluwapo@gmail.com>
2022-06-08 03:38:41 +0100

    unable to figure out why my search emgine isnt working

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,21 +1,34 @@
 import React from "react";
-import SearchEngine from "./SearchEngine";
 import MainDisplay from "./MainDisplay";
-import Forecast from "./Forecast";
 import "./MainContainer.css";
 
 export default function MainContainer() {
+/*
+    const [ready, setReady] = useState(false);
+    const[weatherObj, setWeatherObj] = useState({});
+    function getApi(response) {
+        setWeatherObj(response.data.main);
+        setReady(true);
+        }
+    }
+    if(ready) {
+        */
     return (
         <div className="MainContainer">
-            <div class="row">
-                <div class= "col-md-7">
-                    <SearchEngine />  
-                    <MainDisplay /> 
-                </div>
-                <div class= "col-md-5">
-                    <Forecast />
-                </div>
+            <MainDisplay />
+            <div className="copyright">Weather App is Designed by AnuSanya and 
+            <a href="https://github.com/Anu-022/react-weather-app"> opensourced</a>
             </div>
         </div>
     )
 }
+/*
+else {
+const apiKey = "a2cad1909b029ab4483669f9e477787b";
+let city ='Lagos';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(getApi);
+ 
+ return "Loading..."
+}
+*/
--- src/MainDisplay.js
+++ src/MainDisplay.js
@@ -1,29 +1,109 @@
-import React from "react";
+import React,{useState} from "react";
 import "./MainDisplay.css";
+import axios from "axios";
 import ReactAnimatedWeather from 'react-animated-weather';
+import Forecast from "./Forecast";
 
-export default function MainDisplay (){
-    return (
-        <div className="MainDisplay" mt-3>
-            <ul>
-                <li>Saturday</li>
-                <li>May 19, 2022</li>
-                <li className="weather-description mt-3 text-center">Mostly Cloudy</li>
-            </ul>
-            <div className="weather-icon text-center">
-            <ReactAnimatedWeather icon= {"CLOUDY"} color={"yellow"} size= {150} animate={false} />
-            </div>
-            <ul>
-            <li>Humidity: 80%</li>
-            <li>Wind: 3.5 km/hr</li>
-            </ul>
-            <div>
-            <span className="temperature">9</span>
-            <span className="units">°C</span>
-            </div>
-            
-            <p>Abuja, Nigeria</p>
-            <p className="copyright">Weather App is Designed by AnuSanya & <a href="https://github.com/Anu-022/react-weather-app">opensourced</a></p>   
+export default function MainDisplay() {
+    const [ready, setReady] = useState(false);
+    const [city, setCity] = useState(city);
+    const [temperature, setTemperature] = useState(null);
+    const [humidity, setHumidity] = useState(null);
+    const [wind, setWind] = useState(null);
+    
+    // function to display temperature, humidity and wind values from the openweather api
+    function handleResponse(response) {
+        console.log(response.data);
+        setTemperature(Math.round(response.data.main.temp));
+        setHumidity(response.data.main.humidity);
+        setWind(Math.round(response.data.wind.speed));
+        setReady(true);
+    }
+    
+     //sets city variable to the query typed in by the user.
+     function handleSubmit(event) {
+      event.preventDefault();
+      setCity(city);
+     }
+     
+     function changeCity(event) {
+         event.preventDefault();
+         setCity(event.target.value);
+     }
+     // function to display the current date and year
+     function formatDate() {
+         let today = new Date();
+         let year = today.getFullYear();
+         let date = today.getDate();         
+         let months =["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
+         let month = months[today.getMonth()];
+         return `${month} ${date}, ${year}`;
+       }
+
+       //code to get the current day
+       let today = new Date();
+       let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+       let day = days[today.getDay()];
+
+       
+         
+       // convert celsius to Fahrenheit
+        function showFahrenheit(event) {
+            event.preventDefault();
+            setTemperature(Math.round(((temperature*9) / 5 +32)));
+        }
+       
+        //convert fahreheit to celsius
+        function showCelsius(event) {
+            event.preventDefault();
+            setTemperature(temperature);
+        }
+       //API calls to the openweather api.
+       const apiKey = "a2cad1909b029ab4483669f9e477787b";
+       let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+
+      if(ready) {
+          return (
+              <div className="MainDisplay" mt-3>
+                  <div className="row">
+                      <div className= "col-md-7 pt-4">
+                           <form>
+                               <input type= "search" placeholder="Search a city..." autoFocus= "on" onChange ={changeCity}/>
+                               <button type="submit" onClick={handleSubmit}>Search</button>
+                           </form>
+                           <ul>
+                               <li>{day}</li>
+                               <li>{formatDate()}</li>
+                               <li>{city}</li>
+                               <li className="weather-description mt-3 text-center">Mostly Cloudy</li>
+                           </ul>
+                           <div className="weather-icon text-center">
+                           <ReactAnimatedWeather icon= {"CLOUDY"} color={"yellow"} size= {150} animate={false} />
+                           </div>
+                   <ul>
+                      <li>Humidity: {humidity}%</li>
+                      <li>Wind: {wind} km/hr</li>
+                  </ul>
+                  <div>
+                       <span className="temp-style">{temperature}</span>
+                       <span className="units"><a href="#" onClick = {showCelsius}>°C</a> | <a href="#" onClick={showFahrenheit}>°F</a></span>
+                 </div>
+                    <p>Lagos, Nigeria</p>  
+               </div>
+               <div className= "col-md-5">
+                    <Forecast />
+               </div>
+           </div>
         </div>
     )
+}
+  else {
+    const apiKey = "a2cad1909b029ab4483669f9e477787b";
+    let city ='Lagos';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+     axios.get(apiUrl).then(handleResponse);
+     
+     return "Loading...";
+}
 }
